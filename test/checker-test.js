9d5d55f45b999278e2330b745d45efcb8b55d71b
azu <azuciao@gmail.com>
2015-03-30 18:52:01 +0900

    test: add checker tests

--- test/checker-test.js
+++ test/checker-test.js
@@ -11,8 +11,29 @@ describe("checker-test", function () {
                     name: "@private/module"
                 };
             });
-            it("should return true", function () {
-                assert(checker.validateScopedPackage(pkg));
+            it("should return empty array - No Error!", function () {
+                var errors = checker.validateScopedPackage(pkg);
+                assert(errors.length === 0);
+            });
+        });
+        context("when package name is undefined", function () {
+            beforeEach(function () {
+                pkg = {};
+            });
+            it("should return [error] - has package.name error", function () {
+                var errors = checker.validateScopedPackage(pkg);
+                assert(errors.length >= 1);// will other errors contain
+            });
+        });
+        context("when package name is not start with @", function () {
+            beforeEach(function () {
+                pkg = {
+                    name: "normal-package"
+                };
+            });
+            it("should return [error] - has package.name error", function () {
+                var errors = checker.validateScopedPackage(pkg);
+                assert(errors.length === 1);
             });
         });
     });
